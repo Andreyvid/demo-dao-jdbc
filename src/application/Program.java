943f0b34c30268b805be0a5f6a89355bc9e80fe2
Andreyvid Dutra <andreyviddutra@gmail.com>
2025-07-21 15:37:01 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,13 +12,9 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(1800.0, (sdf.parse("25/02/2002")), obj, "andreyviddutra@gmail.com", 2, "Andreyvid");
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
