12b7f236b7ac1bae3d4e8b85317f23ecf41537b3
Andreyvid Dutra <andreyviddutra@gmail.com>
2025-07-21 21:00:30 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,7 @@ import model.entities.Seller;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
@@ -17,6 +18,15 @@ public class Program {
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println();
+
+        System.out.println("=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
     }
 }
