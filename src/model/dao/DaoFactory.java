40dd836df6fc9b5103feef8809a06f2d4ff51738
Andreyvid Dutra <andreyviddutra@gmail.com>
2025-07-22 13:31:28 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,11 +2,16 @@ package model.dao;
 
 import db.DB;
 import model.impl.SellerDaoJDBC;
+import model.impl.DepartmentDaoJDBC;
 
 public class DaoFactory {
 
-    public static SellerDao createSellerDao(){
+    public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection()) {
         };
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
